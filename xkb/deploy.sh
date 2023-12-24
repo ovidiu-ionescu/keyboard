4645aa83eda28de80586c24f844a9c00a727998c
Ovidiu Ionescu <Ovidiu Ionescu ovidiu@ionescu.net>
2023-12-24 15:39:22 +0100

    Added another Japanese quote Script modifies keyboard files and only replaces the ro one Driver adapted to the new kernel

--- xkb/deploy.sh
+++ xkb/deploy.sh
@@ -4,5 +4,6 @@ set -x
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 DEST=/usr/share/X11/xkb/
-sudo cp -r keycodes $DEST
-sudo cp -r symbols $DEST
+sudo sed -i 's/FK\(\(1[3-9]\)\|\(2[0-9]\)\)> =/FX\1> =/g' $DEST/keycodes/evdev
+sudo sed -i '/<STOP\|<AGAI\|<FRNT\|<COPY\|<PAST/d' $DEST/symbols/inet
+sudo cp symbols/ro $DEST/symbols/ro
